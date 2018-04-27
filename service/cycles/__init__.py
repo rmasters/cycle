faecda546bf4797d6ef933a7a37c29e64388e008
Ross <ross@rossmasters.com>
2018-04-27 22:54:44 +0100

    it works!

--- service/cycles/__init__.py
+++ service/cycles/__init__.py
@@ -1,119 +1,100 @@
+import math
 from datetime import datetime, timedelta
+from queue import Queue, Empty
 import signal
 import sys
 import time
 import threading
-import serial
+from serial import Serial
+
+def serial_reader(serial, start_time, revolutions):
+    while True:
+        # blocking until newline or EOF
+        line = serial.readline().decode()
+
+        # Convert milliseconds-since-arduino-start to a datetime
+        millis = int(line.strip())
+        rev_timestamp = start_time + timedelta(milliseconds=millis)
+        #print(rev_timestamp.strftime('%H:%M:%S.%f'))
+
+        revolutions.append(rev_timestamp)
 
 
-class EventBus:
-    def __init__(self):
-        self.hooks = {}
+def calcs(revolutions):
+    while True:
+        if len(revolutions) < 2:
+            continue
 
-    def on_event(self, event, hook_fn):
-        event_name = self.event_name(event)
-        if event_name not in self.hooks:
-            self.hooks[event_name] = []
-        self.hooks[event_name].append(hook_fn)
+        wheel_radius_cm = 13.0
+        wheel_circumference_cm = 2 * math.pi * wheel_radius_cm
 
-    def emit(self, event):
-        event_name = self.event_name(event)
-        if event_name in self.hooks:
-            for hook in self.hooks[event_name]:
-                hook(event)
+        rotation_distance_km = wheel_circumference_cm / 100000
 
-    def event_name(self, v):
-        return v.__name__ if type(v) == type else type(v).__name__
+        def get_rpm(revolutions, now, window):
+            threshold = now - window
+            rpm = 0.0
 
+            stamps = []
+            for ts in reversed(revolutions):
+                if ts < threshold:
+                    break
+                stamps.append(ts)
 
-class RevolutionEvent:
-    name = 'revolution'
+            if len(stamps) < 2:
+                return None, 0.0
 
-    def __init__(self, timestamp):
-        self.timestamp = timestamp
+            elapsed = 0.0
+            # remember stamps is reversed
+            for idx, ts in enumerate(stamps[1:]):
+                elapsed += (stamps[idx] - stamps[idx-1]).total_seconds()
+            elapsed = elapsed / (len(stamps) - 1)
 
-
-class RevolutionPrinterListener:
-    """Event listener that prints when revolutions occur"""
-    def __init__(self, out_file):
-        self.out_file = out_file
-
-    def on_revolution(self, event):
-        self.out_file.write("Revolution @ %s\n" % event.timestamp.strftime('%H:%M:%S.%f'))
+            return elapsed, 1 / elapsed * 60
 
 
-class SerialReaderThread(threading.Thread):
-    """Reads revolution lines from the serial port and emits events"""
-    def __init__(self, device, baud, event_bus):
-        self.serial = serial.Serial(device, baud)
-        self.stopper = threading.Event()
-        self.events = event_bus
+        elapsed, rpm = get_rpm(revolutions, datetime.now(), timedelta(seconds=5))
+        if elapsed is None:
+            print("STOPPED")
+            time.sleep(1)
+            continue
+       
+        km_per_sec = rotation_distance_km / elapsed
+        km_per_hr = km_per_sec * 3600
 
-        super(SerialReaderThread, self).__init__(name='serial-reader')
+        # 1 = 1 complete rotation (as we have 1 sensor)
+        # 60 = desired 'per' window (i.e. 60 seconds)
+        rpm = 1 / elapsed * 60
 
-    def run(self):
-        start_time = datetime.now()
-        #self.serial.open()
-        while True:
-            if self.stopped():
-                break
+        rotations = len(revolutions)
+        distance_travelled_km = rotation_distance_km * rotations
 
-            line = self.serial.readline().decode()
-            self.process_line(line, start_time)
+        secs_per_km = 1 / km_per_sec
+        pace = timedelta(seconds=secs_per_km)
 
-        #self.serial.close()
-
-    def process_line(self, line, start_time):
-        # Incoming lines are number of milliseconds elapsed since Arduino
-        # program started running. In our case, we can base this off the time
-        # we opened the serial connection. In practice, this overflows after
-        # around 50 days.
-        milliseconds = int(line.strip())
-        revolution_time = start_time + timedelta(milliseconds=milliseconds)
-
-        self.events.emit(RevolutionEvent(revolution_time))
-
-    def stop(self):
-        self.stopper.set()
-
-    def stopped(self):
-        return self.stopper.is_set()
-
-
-class Cycle:
-    def __init__(self, serial_reader):
-        self.serial_reader = serial_reader
-
-    def start(self):
-        self.serial_reader.start()
-
-        # Register signal catchers
-        signal.signal(signal.SIGINT, self.stop)
-        signal.signal(signal.SIGTERM, self.stop)
-
-    def stop(self, *args):
-        self.serial_reader.stop()
-
-        self.serial_reader.join()
+        print("%.2f km/h\t%.1f RPM\t%.2f KM\t%s/km" % (km_per_hr, rpm, distance_travelled_km, str(pace)))
+        time.sleep(1)
 
 
 def main():
-    revolution_printer = RevolutionPrinterListener(sys.stdout)
+    serial = Serial('/dev/ttyACM0', 9600)
+    start_time = datetime.now()
 
-    event_bus = EventBus()
-    event_bus.on_event(RevolutionEvent, revolution_printer.on_revolution)
+    revolutions = []
 
-    # mac: /dev/ttyUSB0
-    # rpi: /dev/ttyACM0
-    serial_reader = SerialReaderThread('/dev/ttyACM0', 9600, event_bus)
-    cycle = Cycle(serial_reader)
+    # Kick off the reader
+    reader = threading.Thread(target=serial_reader, args=(serial, start_time, revolutions))
+    reader.daemon = True
+    reader.start()
+
+    rpm = threading.Thread(target=calcs, args=(revolutions,))
+    rpm.daemon = True
+    rpm.start()
 
     try:
-        cycle.start()
+        while True:
+            reader.join(1)
     except KeyboardInterrupt:
-        print("Stopping...")
-        cycle.stop()
-
-
+        print("Stopping")
+    
 main()
 
