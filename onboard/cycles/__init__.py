eef3a0e4006e9d52fb4f0508066e702f99a97595
Ross Masters <ross@rossmasters.com>
2018-05-03 21:35:56 +0100

    Rewrite file logger to use multiple files, per session

--- onboard/cycles/__init__.py
+++ onboard/cycles/__init__.py
@@ -5,16 +5,18 @@ import threading
 from serial import Serial
 import os
 
-from .events import EventManager, RevolutionEvent
+from .events import EventManager, RevolutionEvent, SessionStartEvent, SessionEndEvent
 from .sensors import GpioReader, SerialReader
-from .logger import file_persister
+from .logger import FileLogger
 from .reporter import reporter_thread
 from .analysis import RevolutionCalculator
 from .stream import websocket_server
+from .sessions import SessionWatcher
 
 def main():
-    persist_file_path = os.path.expanduser('~/cycles.txt')
-    persist_file = open(persist_file_path, 'a', buffering=1)
+    persist_path = os.path.expanduser('~/cycles')
+    if not os.path.isdir(persist_path):
+        os.makedirs(persist_path)
 
     log_queue = Queue()
 
@@ -29,6 +31,11 @@ def main():
     events.on(RevolutionEvent, lambda e: log_queue.put(e))
     events.on(RevolutionEvent, lambda e: calc.record_revolutions(e.timestamp))
 
+    events.on(SessionStartEvent, lambda e: log_queue.put(e))
+    events.on(SessionEndEvent, lambda e: log_queue.put(e))
+
+    #events.on(PauseEvent, lambda e: log_queue.put(e))
+
     #serial = Serial('/dev/ttyACM0', 9600)
     #serial = Serial('/dev/ttyUSB0', 9600)
     #serial = Serial('/dev/cu.usbmodem1411', 9600)
@@ -38,7 +45,7 @@ def main():
     reader.daemon = True
     reader.start()
 
-    logger = threading.Thread(target=file_persister, args=(log_queue, persist_file))
+    logger = FileLogger(log_queue, persist_path)
     logger.daemon = True
     logger.start()
 
@@ -55,6 +62,4 @@ def main():
             reader.join(1)
     except KeyboardInterrupt:
         print("Stopping")
-
-    persist_file.close()
 
--- onboard/cycles/logger.py
+++ onboard/cycles/logger.py
@@ -1,27 +1,55 @@
+import os
 import time
-from .events import StopEvent, RevolutionEvent
+import threading
+from .events import StopEvent, RevolutionEvent, SessionStartEvent, SessionEndEvent
 
 
-def file_persister(queue, persist_file):
-    while True:
-        # TODO: timeout here so that the StopEvent works?
-        event = queue.get()
+class FileLogger(threading.Thread):
+    def __init__(self, queue, persist_dir):
+        super(FileLogger, self).__init__()
 
-        # Catch break-out tasks
-        if isinstance(event, StopEvent):
-            queue.task_done()
-            break
+        self.queue = queue
+        self.persist_dir = persist_dir
 
-        if isinstance(event, RevolutionEvent):
-            # Write a millisecond-accurate, UTC timestamp to the file
-            # Datetime -> timestamp with milliseconds https://stackoverflow.com/a/8159893
-            dt = event.timestamp
-            timestamp = int((time.mktime(dt.utctimetuple()) + dt.microsecond / 1000000.0)*1000)
+        self.persist_file = None
 
-            persist_file.write(str(timestamp) + "\n")
+    def run(self):
+        while True:
+            event = self.queue.get()
 
-            queue.task_done()
-            continue
+            if isinstance(event, RevolutionEvent):
+                self.on_revolution(event)
 
-        queue.task_done()
+            if isinstance(event, SessionStartEvent):
+                self.on_session_start(event)
+
+            if isinstance(event, SessionEndEvent):
+                self.on_session_stop(event)
+
+            self.queue.task_done()
+
+    def on_revolution(self, event):
+        # Write a millisecond-accurate, UTC timestamp to the file
+        # Datetime -> timestamp with milliseconds https://stackoverflow.com/a/8159893
+        dt = event.timestamp
+        timestamp = int((time.mktime(dt.utctimetuple()) + dt.microsecond / 1000000.0)*1000)
+
+        if self.persist_file is None:
+            self.open_persist_file(event.timestamp)
+
+        self.persist_file.write(str(timestamp) + "\n")
+
+    def on_session_start(self, event):
+        self.open_persist_file(event.started_at)
+
+    def open_persist_file(self, timestamp):
+        filename = "cyclelog-%s.log" % timestamp.strftime("%Y%m%d%H%M%S")
+        persist_file_path = os.path.join(self.persist_dir, filename)
+
+        self.persist_file = open(persist_file_path, 'a')
+
+    def on_session_stop(self, event):
+        if self.persist_file:
+            self.persist_file.close()
+            self.persist_file = None
 
